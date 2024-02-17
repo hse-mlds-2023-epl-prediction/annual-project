3f23279d88c517eec80fef33d3938cbb782eb224
Chewye <werfa13q@gmail.com>
2024-02-17 12:43:46 +0500

    add test game_tomorrow and gems_ten

--- services/bot/src/test_main.py
+++ services/bot/src/test_main.py
@@ -4,7 +4,7 @@ import asyncio
 from aiogram.filters import Command
 from aiogram.methods import AnswerCallbackQuery
 from aiogram.methods import SendMessage
-from main import games_today
+from main import games_today, games_tomorrow, games_ten
 from pytest_mock import MockerFixture
 from aiogram_tests import MockedBot
 import api_client
@@ -12,11 +12,11 @@ from aiogram_tests.handler import CallbackQueryHandler
 from aiogram_tests.handler import MessageHandler
 from aiogram_tests.types.dataset import CALLBACK_QUERY
 from aiogram_tests.types.dataset import MESSAGE
-
+from aiogram_tests.bot import MockedBot  # Add this line
 
 @pytest.mark.asyncio
 async def test_games_today(mocker: MockerFixture):
-    mocker.patch('bot.make_request', return_value=[])
+    mocker.patch('main.make_request', return_value=[])
     requester = MockedBot(request_handler=MessageHandler(games_today, auto_mock_success=False))
     calls = await requester.query(MESSAGE.as_object(text="games_today"))
     answer_message = calls.send_message.fetchone().text
@@ -25,9 +25,59 @@ async def test_games_today(mocker: MockerFixture):
 
     assert answer_message == expectedResult
 
-    mocker.patch('bot.make_request', return_value=[{'Home': '1', 'Away': '2', 'Ground': '3'}])
+    mocker.patch('main.make_request', return_value=[{'Home': '1', 'Away': '2', 'Ground': '3'}])
     requester = MockedBot(request_handler=MessageHandler(games_today, auto_mock_success=False))
     calls = await requester.query(MESSAGE.as_object(text="games_today"))
+    answer_message = calls.send_message.fetchone().text
+
+    expectedResult = '''<pre>+------+------+--------+
+| Home | Away | Ground |
++------+------+--------+
+| 1    | 2    |      3 |
++------+------+--------+</pre>'''
+
+    assert answer_message == expectedResult
+
+
+@pytest.mark.asyncio
+async def test_games_tomorrow(mocker: MockerFixture):
+    mocker.patch('main.make_request', return_value=[])
+    requester = MockedBot(request_handler=MessageHandler(games_tomorrow, auto_mock_success=False))
+    calls = await requester.query(MESSAGE.as_object(text="games_tomorrow"))
+    answer_message = calls.send_message.fetchone().text
+
+    expectedResult = 'No games tomorrow'
+
+    assert answer_message == expectedResult
+
+    mocker.patch('main.make_request', return_value=[{'Home': '1', 'Away': '2', 'Ground': '3'}])
+    requester = MockedBot(request_handler=MessageHandler(games_tomorrow, auto_mock_success=False))
+    calls = await requester.query(MESSAGE.as_object(text="games_tomorrow"))
+    answer_message = calls.send_message.fetchone().text
+
+    expectedResult = '''<pre>+------+------+--------+
+| Home | Away | Ground |
++------+------+--------+
+| 1    | 2    |      3 |
++------+------+--------+</pre>'''
+
+    assert answer_message == expectedResult
+
+
+@pytest.mark.asyncio
+async def test_games_ten(mocker: MockerFixture):
+    mocker.patch('main.make_request', return_value=[])
+    requester = MockedBot(request_handler=MessageHandler(games_ten, auto_mock_success=False))
+    calls = await requester.query(MESSAGE.as_object(text="games_ten"))
+    answer_message = calls.send_message.fetchone().text
+
+    expectedResult = 'No games tomorrow'
+
+    assert answer_message == expectedResult
+
+    mocker.patch('main.make_request', return_value=[{'Home': '1', 'Away': '2', 'Ground': '3'}])
+    requester = MockedBot(request_handler=MessageHandler(games_ten, auto_mock_success=False))
+    calls = await requester.query(MESSAGE.as_object(text="games_ten"))
     answer_message = calls.send_message.fetchone().text
 
     expectedResult = '''<pre>+------+------+--------+
