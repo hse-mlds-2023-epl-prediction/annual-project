81099f80f29342e849b09e883a7355dc02520e3f
Chewye <werfa13q@gmail.com>
2024-04-07 18:34:06 +0500

    src api refactoring flake8

--- services/api/src/stats.py
+++ services/api/src/stats.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 from pydantic import BaseModel
 
+
 class StatInfo(BaseModel):
     team_name: str
     avg_score_home: float
@@ -9,15 +10,18 @@ class StatInfo(BaseModel):
     avg_score_away: float
     avg_yellow_cards_count_away: float
 
+
 async def stats():
-    df = pd.read_csv(os.path.join(os.path.dirname(__file__)) + '/data/2023_2024_matches.csv')
+    df = pd.read_csv(os.path.join(
+        os.path.dirname(__file__)) + '/data/2023_2024_matches.csv')
 
     avg_scores_home = df.groupby('home_team_name')['home_team_score'].mean()
     avg_scores_away = df.groupby('away_team_name')['away_team_score'].mean()
 
-    avg_yellow_cards_count_home = df.groupby('home_team_name')['home_team_yellow_cards_count'].mean()
-    avg_yellow_cards_count_away = df.groupby('away_team_name')['away_team_yellow_cards_count'].mean()
-
+    avg_yellow_cards_count_home = df.groupby(
+        'home_team_name')['home_team_yellow_cards_count'].mean()
+    avg_yellow_cards_count_away = df.groupby(
+        'away_team_name')['away_team_yellow_cards_count'].mean()
 
     df_home = pd.DataFrame({
         'team_name': avg_scores_home.index,
